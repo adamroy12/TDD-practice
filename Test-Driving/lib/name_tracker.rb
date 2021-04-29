65d249a0937b5bfec3e33240af4600ae2d060586
Adam R <adamroyal12@gmail.com>
2021-04-29 14:20:11 +0100

    made it work

--- Test-Driving/lib/name_tracker.rb
+++ Test-Driving/lib/name_tracker.rb
@@ -2,14 +2,27 @@ class NameTracker
     def initialize
     @names = []
     end
-   
+
+    def names
+        @names
+    end
+
     def add_name(name)
         @names << (name)
-        puts "Name added: #{name}"
+        p "Name added: #{name}"
     end
 
-    def check_names 
-        puts @names
+    def check_names
+        p @names.join(", ")
     end
-
 end
+
+n = NameTracker.new
+
+n.add_name("Adam")
+
+n.check_names
+
+n.add_name("Jim")
+
+n.check_names
--- Test-Driving/spec/name_tracker_spec.rb
+++ Test-Driving/spec/name_tracker_spec.rb
@@ -2,15 +2,18 @@ require "name_tracker.rb"
     
 describe NameTracker do
     it "tells us a name is added" do
-        expect(subject.add_name("name")) == "Name added: name"
+        expect(subject.add_name("name")).to eq "Name added: name"
     end
 
     it "adds a name" do
-        subject.add_name("name")
-        expect @names == "name"
+        name = "name"
+        subject.add_name(name)
+        expect(subject.names).to eq [name]
     end
 
     it "allows you to check your names" do
-        expect(subject.check_names).to eq @names
+        name = "name"
+        subject.add_name(name)
+        expect(subject.check_names).to eq "name"
     end
 end
